3dde5a0c36c0969391370e0a4229d68a44b4aee4
henriquekhayashi <53438750+henriquekhayashi@users.noreply.github.com>
2022-05-29 22:02:42 -0300

    Delete design-patterns-spring directory

--- design-patterns-spring/src/main/java/com/example/designpatternsspring/model/EnderecoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.designpatternsspring.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EnderecoRepository extends CrudRepository<Endereco, String> {
-
-}
